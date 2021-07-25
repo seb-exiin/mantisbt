ab2a05b07c82fec81c6d5ad8a561261594dbeba6
Sebastien <34646501+seb-exiin@users.noreply.github.com>
2021-07-26 00:27:09 +0200

    fix root issue

--- wiki/conf/local.protected.php
+++ wiki/conf/local.protected.php
@@ -3,7 +3,10 @@
 $protocol = strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https'?'https':'http';
 
 #Path to the MantisBT root directory on the server
-define( 'MANTIS_ROOT', '../' );
+ $droot = $_SERVER['DOCUMENT_ROOT'];
+
+define( 'MANTIS_ROOT', $droot.'/' );
+//echo MANTIS_ROOT." ====> MANTIS_ROOT";
 include( MANTIS_ROOT . 'core.php' );
 
 $conf['useacl']      = 1;
